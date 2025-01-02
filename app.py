319f71dbc945dd330ebb517ee93a1f84c7ffd7cb
arun <arung6665@gmail.com>
2025-01-02 12:47:27 -0500

    Final 1.0

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_restful import Api
 from models import db
 from config import Config
+from flask_cors import CORS
 from resources.whitelist import WhitelistResource
 from resources.blacklist import BlacklistResource
 
 app = Flask(__name__)
+CORS(app) 
 app.config.from_object(Config)
 
 # Initialize SQLAlchemy (without Flask-Migrate for now)
@@ -16,9 +18,33 @@ api = Api(app)
 api.add_resource(WhitelistResource, '/whitelist')
 api.add_resource(BlacklistResource, '/blacklist')
 
-print("Database URI:", Config.SQLALCHEMY_DATABASE_URI)
-
-
-
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+''''from flask import Flask
+from flask_restful import Api
+from flask_migrate import Migrate
+from models import db
+from config import Config
+from resources.whitelist import WhitelistResource
+from resources.blacklist import BlacklistResource
+from resources.reports import UsageReportResource
+from resources.filtered_whitelist import FilteredWhitelistResource
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+# Initialize SQLAlchemy and Flask-Migrate
+db.init_app(app)
+migrate = Migrate(app, db)
+
+# Initialize Flask-RESTful API
+api = Api(app)
+api.add_resource(WhitelistResource, '/whitelist')
+api.add_resource(BlacklistResource, '/blacklist')
+api.add_resource(UsageReportResource, '/usage_report')
+api.add_resource(FilteredWhitelistResource, '/filtered_whitelist') 
+
+if __name__ == '__main__':
+    app.run()''''''
